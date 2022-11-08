b4c5ca044164248fe829aefeb6b8738d47887535
Brandon They <theyb5220@gmail.com>
2022-11-08 15:23:15 -0600

    Added trimming to preview

--- src/components/EmailPreviewCard/EmailPreviewCard.jsx
+++ src/components/EmailPreviewCard/EmailPreviewCard.jsx
@@ -3,11 +3,20 @@ import "./EmailPreviewCard.scss"
 const EmailPreviewCard = (props) => {
 
     const {userName, header, content, setChosenEmailIndex, index} = props;
+
+    //finding the ending of the nearest sentence to 100 characters
+    const lastSentenceIndex = content?.indexOf(".", 100) + 1;
+
+    let trimmedContent = content;
+    if(lastSentenceIndex > 100){
+        trimmedContent = content?.substring(0, lastSentenceIndex);
+    }
+
     return(
             <section onClick={() => {setChosenEmailIndex(index)}} className="emailcard">
                 <p className="userName">{userName}</p>
                 <h4 className="Header">{header}</h4> 
-                <p className="content">{content}</p>  
+                <p className="content">{trimmedContent}</p> 
             </section>      
     )
 }
