879b83440c2c2c4aaf5dc9ba23706840cd56dffe
Brandon They <theyb5220@gmail.com>
2022-11-11 13:14:59 -0600

    Working on filtering

--- src/containers/EmailPreview/EmailPreview.jsx
+++ src/containers/EmailPreview/EmailPreview.jsx
@@ -5,15 +5,66 @@ import { useEffect, useState } from "react";
 import Multiselect from "multiselect-react-dropdown";
 const EmailPreview = (props) => {
     const {emailsArr} = props;
-
+    let renderedEmails;
+    const [selectedFilters, setSelectedFilters] = useState()
+    const [filteredEmails, setFilteredEmails] = useState(emailsArr)
     const [chosenEmail, setChosenEmail] = useState();
     const [chosenEmailIndex, setChosenEmailIndex] = useState();
 
-    const renderedEmails = emailsArr.map((email,index) => (
-        <>
-            <EmailPreviewCard userName={email.userName} header={email.header} content={email.content} setChosenEmailIndex={setChosenEmailIndex} index={index}/>
-        </>
-    ))
+    const filterItems = [
+        {group: "Importance", key: "High"},
+        {group: "Importance", key: "Medium"},
+        {group: "Importance", key: "Low"},
+        {group: "Date", key: "Most Recent"},
+        {group: "Date", key: "Oldest"},
+        {group: "Type", key: "Personal"},
+        {group: "Type", key: "Work"}
+    ]
+
+    const selectHandler = (event) =>{
+        setSelectedFilters(event)
+        if(selectedFilters){
+            let groups = [];
+            let filters = [];
+            for(let i = 0; i < selectedFilters.length; i++){
+                groups.push(selectedFilters[i].group.toLowerCase())
+                filters.push(selectedFilters[i].key.toLowerCase())
+            }
+            console.log(selectedFilters)
+            console.log(selectedFilters[0].key)
+            filteredEmails.map(email => {
+                console.log(groups)
+                console.log(filters)
+                console.log(email[groups[0]]) //accessing email keys using the group categories and 
+                //email[groups[0]] == filters[0]
+                // console.log(email.selectedFilters[0].group.toLowerCase())
+            })
+            // setFilteredEmails(filteredEmails.map(email => {
+            //    if(email.importance == selectedFilters[0].key){
+            //     return email
+            //    }
+            // }))
+        }
+    }
+    
+    if(filteredEmails){
+        renderedEmails = filteredEmails.map((email,index) => (
+            <>
+                <EmailPreviewCard userName={email.userName} header={email.header} content={email.content} setChosenEmailIndex={setChosenEmailIndex} index={index}/>
+            </>
+        ))
+    }
+    else {
+        renderedEmails = emailsArr.map((email,index) => (
+            <>
+                <EmailPreviewCard userName={email.userName} header={email.header} content={email.content} setChosenEmailIndex={setChosenEmailIndex} index={index}/>
+            </>
+        ))
+    }
+
+    useEffect(() => {
+        selectHandler()
+    }, [selectedFilters])
 
     useEffect(() => {
         setChosenEmail(emailsArr[chosenEmailIndex])
@@ -26,39 +77,13 @@ const EmailPreview = (props) => {
                     <button className="compose-btn">Compose +</button>
                     <section className="filter-dropdown">
                         <Multiselect 
+                        
                             placeholder="Filter by"
                             displayValue="key"
                             groupBy="group"
-                            options={[
-                                {
-                                    group: "Importance",
-                                    key: "High"
-                                },
-                                {
-                                    group: "Importance",
-                                    key: "Medium"
-                                },
-                                {
-                                    group: "Importance",
-                                    key: "Low"
-                                },
-                                {
-                                    group: "Date",
-                                    key: "Most Recent"
-                                },
-                                {
-                                    group: "Date",
-                                    key: "Oldest"
-                                },
-                                {
-                                    group: "Type",
-                                    key: "Personal"
-                                },
-                                {
-                                    group: "Type",
-                                    key: "Work"
-                                }
-                            ]}
+                            options={filterItems}
+                            onSelect={selectHandler}
+                            onRemove={selectHandler}
                             showCheckbox
                         />
                     </section>
